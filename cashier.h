e4013b242b209155b6d27b693f33cf710ff413d9
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-06-01 18:53:01 -0700

    added basic methods to class

--- cashier.h
+++ cashier.h
@@ -3,6 +3,17 @@
 
 #include "employee.cpp"
 
-class Cashier : public Employee {};
+class Cashier : public Employee {
+    private:
+        int pin_;
+    public:
+        Cashier();
+        Cashier(const int& pin, const std::string& firstName, const std::string& lastName);
+        Cashier(const Cashier& other);
+        ~Cashier();
+        Cashier& operator=(const Cashier& other);
+        int getPin() const;
+        void setPin(const int& pin);
+};
 
 #endif
