7c975bf61ec4e7d3b7311420f81d0fd6a6dee80b
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-06-02 23:11:09 -0700

    added methods

--- register.h
+++ register.h
@@ -1,6 +1,31 @@
 #ifndef REGISTER_H
 #define REGISTER_H
 
-class Register {};
+#include "cashier.cpp"
+#include "receipt.cpp"
+
+class Register {
+    private:
+        Cashier cashier_;
+        std::vector<Product> productList_;
+        Receipt receipt_;
+        float balance_due_;
+        float tender_received_;
+        float change_due_;
+        float deposit_balance_;
+    public:
+        Register();
+        Register(const float& balance);
+        Register(const Register& other);
+        ~Register();
+        Register& operator=(const Register& other);
+        int getDepositBalance() const;
+        float getProductPrice(const Product& product);
+        void addProduct(const Product& product);
+        void removeProduct(const Product& product);
+        void checkout(const float& tender_received);
+        void printReceipt();
+        float close();
+};
 
 #endif
