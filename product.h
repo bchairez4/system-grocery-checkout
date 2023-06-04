dade3b55bf116af731fa712e6ed13b131f8d9c87
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-06-03 19:59:38 -0700

    replaced ID_ with department_
    department_ has a heavier weight in terms of variable importance for the intents and purposes of this project

--- product.h
+++ product.h
@@ -5,19 +5,19 @@
 
 class Product {
     private:
-        int ID_;
+        std::string department_;
         std::string name_;
         float price_;
     public:
         Product();
-        Product(const int& ID, const std::string& name, const float& price);
+        Product(const std::string& department, const std::string& name, const float& price);
         Product(const Product& other);
         ~Product();
         Product& operator=(const Product& other);
-        int getID() const;
+        std::string getDepartment() const;
         std::string getName() const;
         float getPrice() const;
-        void setID(const int& ID);
+        void setDepartment(const std::string& department);
         void setName(const std::string& name);
         void setPrice(const float& price);
 };
