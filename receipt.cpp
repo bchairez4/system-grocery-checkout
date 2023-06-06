f9ede02e73ff05e30a80ec77431d05a5c2defb3f
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-06-05 22:30:47 -0700

    implemented added constructor

--- receipt.cpp
+++ receipt.cpp
@@ -2,6 +2,12 @@
 
 Receipt::Receipt() {}
 
+Receipt::Receipt(const std::vector<Product>& productList) {
+    for (int i = 0; i < productList.size(); ++i) {
+        productList_.push_back(productList[i]);
+    }
+}
+
 Receipt::Receipt(const Receipt& other) {
     productList_.clear();
     for (int i = 0; i < other.productList_.size(); ++i) {
@@ -17,12 +23,6 @@ Receipt& Receipt::operator=(const Receipt& other) {
         productList_.push_back(other.productList_[i]);
     }
     return *this;
-}
-
-void Receipt::create(const std::vector<Product>& productList) {
-    for (int i = 0; i < productList.size(); ++i) {
-        productList_.push_back(productList[i]);
-    }
 }
 
 void Receipt::print() {
