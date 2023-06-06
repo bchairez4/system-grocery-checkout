0de214c4eff894802f81b1b7f9d9fdddee1894d9
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-06-05 21:52:48 -0700

    removed ID_ as a member variable

--- employee.cpp
+++ employee.cpp
@@ -1,27 +1,17 @@
 #include "employee.h"
 
-Employee::Employee() : ID_(-1), firstName_("NULL"), lastName_("NULL") {}
+Employee::Employee() : firstName_("NULL"), lastName_("NULL") {}
 
-Employee::Employee(const std::string& firstName, const std::string& lastName) : firstName_(firstName), lastName_(lastName) {
-    //ID is a random 6 digit number 
-    std::srand((unsigned) time(NULL));
+Employee::Employee(const std::string& firstName, const std::string& lastName) : firstName_(firstName), lastName_(lastName) {}
 
-    ID_ = 1 + (std::rand() % 1000000);
-}
-
-Employee::Employee(const Employee& other) : ID_(other.ID_), firstName_(other.firstName_), lastName_(other.lastName_) {}
+Employee::Employee(const Employee& other) : firstName_(other.firstName_), lastName_(other.lastName_) {}
 
 Employee::~Employee() {}
 
 Employee& Employee::operator=(const Employee& other) {
-    ID_ = other.ID_;
     firstName_ = other.firstName_;
     lastName_ = other.lastName_;
     return *this;
-}
-
-int Employee::getID() const {
-    return ID_;
 }
 
 std::string Employee::getFirstName() const {
@@ -34,10 +24,6 @@ std::string Employee::getLastName() const {
 
 std::string Employee::getFullName() const {
     return (firstName_ + " " + lastName_);
-}
-
-void Employee::setID(const int& ID) {
-    ID_ = ID;
 }
 
 void Employee::setFirstName(const std::string& firstName) {
