62ceba694c69cfb404d0b079f96c99980f5b06ed
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-06-05 21:53:33 -0700

    removed ID_ as a member variable

--- employee.h
+++ employee.h
@@ -2,11 +2,9 @@
 #define EMPLOYEE_H
 
 #include <string>
-#include <cstdlib>  //rand()
 
 class Employee {
     private:
-        int ID_;
         std::string firstName_;
         std::string lastName_;
     public:
@@ -15,11 +13,9 @@ class Employee {
         Employee(const Employee& other);
         ~Employee();
         Employee& operator=(const Employee& other);
-        int getID() const;
         std::string getFirstName() const;
         std::string getLastName() const;
         std::string getFullName() const;
-        void setID(const int& ID);
         void setFirstName(const std::string& firstName);
         void setLastName(const std::string& lastName);
 };
