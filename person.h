768b153af473b78348c1639c9c8d1f411c043ca9
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-06-10 20:05:11 -0700

    implementations moved to header file

--- person.h
+++ person.h
@@ -8,16 +8,39 @@ class Person {
         std::string firstName_;
         std::string lastName_;
     public:
-        Person();
-        Person(const std::string& firstName, const std::string& lastName);
-        Person(const Person& other);
-        ~Person();
-        Person& operator=(const Person& other);
-        std::string getFirstName() const;
-        std::string getLastName() const;
-        std::string getFullName() const;
-        void setFirstName(const std::string& firstName);
-        void setLastName(const std::string& lastName);
+        Person() : firstName_("NULL"), lastName_("NULL") {}
+
+        Person(const std::string& firstName, const std::string& lastName) : firstName_(firstName), lastName_(lastName) {}
+
+        Person(const Person& other) : firstName_(other.firstName_), lastName_(other.lastName_) {}
+
+        ~Person() {}
+
+        Person& operator=(const Person& other) {
+            firstName_ = other.firstName_;
+            lastName_ = other.lastName_;
+            return *this;
+        }
+
+        std::string getFirstName() const {
+            return firstName_;
+        }
+
+        std::string getLastName() const {
+            return lastName_;
+        }
+
+        std::string getFullName() const {
+            return (firstName_ + " " + lastName_);
+        }
+
+        void setFirstName(const std::string& firstName) {
+            firstName_ = firstName;
+        }
+
+        void setLastName(const std::string& lastName) {
+            lastName_ = lastName;
+        }
 };
 
 #endif
