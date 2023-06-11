5b63deeb9f74f35b4ddaa578c5096c3f613ffb63
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-06-10 20:06:12 -0700

    implementations moved to header file

--- receipt.h
+++ receipt.h
@@ -1,7 +1,7 @@
 #ifndef RECEIPT_H
 #define RECEIPT_H
 
-#include "product.cpp"
+#include "product.h"
 #include <iostream>
 #include <vector>
 
@@ -9,12 +9,36 @@ class Receipt {
     private:
         std::vector<Product> productList_;
     public:
-        Receipt();
-        Receipt(const std::vector<Product>& productList);
-        Receipt(const Receipt& other);
-        ~Receipt();
-        Receipt& operator=(const Receipt& other);
-        void print();
+        Receipt() {}
+
+        Receipt(const std::vector<Product>& productList) {
+            for (int i = 0; i < productList.size(); ++i) {
+                productList_.push_back(productList[i]);
+            }
+        }
+
+        Receipt(const Receipt& other) {
+            productList_.clear();
+            for (int i = 0; i < other.productList_.size(); ++i) {
+                productList_.push_back(other.productList_[i]);
+            }
+        }
+
+        ~Receipt() {}
+
+        Receipt& operator=(const Receipt& other) {
+            productList_.clear();
+            for (int i = 0; i < other.productList_.size(); ++i) {
+                productList_.push_back(other.productList_[i]);
+            }
+            return *this;
+        }
+
+        void print() {
+            for (std::vector<Product>::const_iterator it = productList_.begin(); it != productList_.end(); ++it) {
+                std::cout << it->getName() << " " << it->getPrice() << '\n';
+            }
+        }
 };
 
 #endif
