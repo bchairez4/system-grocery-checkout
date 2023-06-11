a4caf32140fbb2e783afe7c520b80e22dddc765a
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-06-10 20:04:26 -0700

    implementation moved to header file

--- lane.h
+++ lane.h
@@ -1,7 +1,7 @@
 #ifndef LANE_H
 #define LANE_H
 
-#include "register.cpp"
+#include "register.h"
 #include <queue>
 
 class Lane {
@@ -10,24 +10,97 @@ class Lane {
         Register reg_;
         std::queue<Product> cart_;
     public:
-        Lane();
-        Lane(const std::vector<Product>& cart);
-        Lane(const Lane& other);
-        ~Lane();
-        Lane& operator=(const Lane& other);
-        Cashier getCurrentCashier() const;
-        Register getRegister() const;
-        std::queue<Product> getCart() const;
-        bool isSignedIn() const;
-        bool isAssigned() const;
-        void signIn(const int& pin);
-        void signOut();
-        void assignRegister(const Cashier& cashier);
-        float closeRegister();
-        void addProductsToLane(const std::vector<Product>& cart);
-        void scanProducts();
-        void removeProduct(const Product& product);
-        void checkout(const float& tenderReceived);
+        Lane() : signedIn_(false) {}
+
+        Lane(const std::vector<Product>& cart) {
+            for (int i = 0; i < cart.size(); ++i) {
+                cart_.push(cart[i]);
+            }
+        }
+
+        Lane(const Lane& other) : signedIn_(other.signedIn_), reg_(other.reg_), cart_(other.cart_) {}
+
+        ~Lane() {}
+
+        Lane& operator=(const Lane& other) {
+            signedIn_ = other.signedIn_;
+            reg_ = other.reg_;
+            cart_ = other.cart_;
+            return *this;
+        }
+
+        Cashier getCurrentCashier() const {
+            return reg_.getCashier();
+        }
+
+        Register getRegister() const {
+            return reg_;
+        }
+
+        std::queue<Product> getCart() const {
+            return cart_;
+        }
+
+        bool isSignedIn() const {
+            return signedIn_;
+        }
+
+        bool isAssigned() const {
+            return reg_.getCashier().getFirstName() != "NULL";
+        }
+
+        void signIn(const int& pin) {
+            if (isAssigned()) {
+                return;
+            }
+
+            if (reg_.getCashier().getPin() == pin) {
+                signedIn_ = true;
+            }
+        }
+
+        void signOut() {
+            if (isAssigned()) {
+                signedIn_ = false;
+            }
+        }
+
+        void assignRegister(const Cashier& cashier) {
+            reg_.setCashier(cashier);
+        }
+
+        float closeRegister() {
+            if (!isAssigned()) {
+                return 0.00f;
+            }
+
+            float deposit = reg_.close();
+
+            return deposit;
+        }
+
+        void addProductsToLane(const std::vector<Product>& cart) {
+            for (std::vector<Product>::const_iterator it = cart.cbegin(); it != cart.cend(); ++it) {
+                cart_.push(*it);
+            }
+        }
+
+        void scanProducts() {
+            while (!cart_.empty()) {
+                reg_.addProduct(cart_.front());
+                cart_.pop();
+            }
+        }
+
+        void removeProduct(const Product& product) {
+            reg_.removeProduct(product);
+        }
+
+        float checkout(const float& tenderReceived) {
+            float income = reg_.checkout(tenderReceived);
+            reg_.printReceipt();
+            return income;
+        }
 };
 
 #endif
