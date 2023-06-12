f8e2164bf36d341ba030d79695b4e898859ed050
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-06-11 20:27:52 -0700

    updated copy constructor

--- customer.h
+++ customer.h
@@ -15,7 +15,8 @@ class Customer : public Person {
         Customer(const std::string& firstName, const std::string& lastName, const int& phoneNumber, const int& rewardPoints)
         : Person(firstName, lastName), phoneNumber_(phoneNumber), rewardPoints_(rewardPoints) {}
 
-        Customer(const Customer& other) : phoneNumber_(other.phoneNumber_), rewardPoints_(other.rewardPoints_) {
+        Customer(const Customer& other) 
+        : Person(other.getFirstName(), other.getLastName()), phoneNumber_(other.phoneNumber_), rewardPoints_(other.rewardPoints_) {
             cart_.clear();
             for (int i = 0; i < other.cart_.size(); ++i) {
                 cart_.push_back(cart_[i]);
